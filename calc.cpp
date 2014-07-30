68e81e977d25e4a4c511fa4a43a8e88f83cb6515
supriya-sudake <supriya.sudake@cumminscollege.in>
2014-07-30 14:29:43 +0530

    Update calc.cpp
    added new method

--- calc.cpp
+++ calc.cpp
@@ -2,5 +2,13 @@
 void main()
 {
   cout<<"welcome to Github";
-  
+  cout<<"Square",<<square();
+}
+
+
+double square(double in_num)
+{
+  double out_num;
+  out_num=in_num*in_num;
+  return out_num;
 }
